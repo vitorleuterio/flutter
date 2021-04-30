8be7f597f370d2ec073d8cceb084cb5825fa1329
Vitor Leuterio <leu_vitor@univem.edu.br>
2021-04-30 20:13:32 -0300

    Primeiro Commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Login Flutter',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
